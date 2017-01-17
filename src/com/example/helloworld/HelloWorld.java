25239d297e133c247ef2bedd7d89f5fc1d4e97d3
Brandon Watson <watsonb3@xavier.edu>
2017-01-17 03:45:00 -0500

    Hello World App

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by bkandon7 on 01/17/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World??");
+    }
+}
